a799c9da5cd2b6de599573370ab8b043fce4a724
nguyencuong88 <hr.bitcode@gmail.com>
2025-10-29 17:14:38 +0700

    ad CI Unit and Acceptance

--- config/config.php
+++ config/config.php
@@ -3,4 +3,4 @@ define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
 define('DB_NAME', getenv('DB_NAME') ?: 'bookstore');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASS') !== false ? getenv('DB_PASS') : '');
-define('APP_BASE_URL', getenv('APP_BASE_URL') ?: 'http://localhost');
+define('APP_BASE_URL', getenv('APP_BASE_URL') ?: 'http://127.0.0.1');
